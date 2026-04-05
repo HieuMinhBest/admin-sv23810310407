00815e6fa4e014b968271a14f7e5636df86656e2
HieuMinhBest <nguyenhieulc2005@gmail.com>
2026-04-05 08:18:44 +0700

    feat: Tạo bảng Categories, Products với tiền tố MSSV sv12345 và Models tương ứng

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $table = 'sv23810310407_products';
+    protected $guarded = [];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
